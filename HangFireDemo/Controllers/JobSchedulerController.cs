83cc6362eb22eb0d9e18210500faf1b8782ad28a
Lenovo <Lenovo@DESKTOP-MRP8P5Q>
2024-05-09 16:04:40 +0500

    Add project files.

---
+++ HangFireDemo/Controllers/JobSchedulerController.cs
@@ -0,0 +1,26 @@
+﻿using Hangfire;
+using HangFireDemo.Helper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HangFireDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobSchedulerController : ControllerBase
+    {
+        string welcomePackage = "HangFire";
+        private IResponseHelper response;
+        public JobSchedulerController(IResponseHelper responseHelper) 
+        { 
+            response = responseHelper;
+        }
+
+        [HttpGet]
+        public IActionResult FireAndForgetJob()
+        {
+            BackgroundJob.Enqueue(() => Console.WriteLine($"Welcome to {welcomePackage}"));
+            return Ok();
+        }
+    }
+}
---
+++ HangFireDemo/Helper/Response.cs
@@ -0,0 +1,21 @@
+﻿namespace HangFireDemo.Helper
+{
+    public interface IResponseHelper
+    {
+        Response GetResponseTemplate();    
+    }
+
+    public class ResponseHelper : IResponseHelper
+    {
+        public Response GetResponseTemplate()
+        {
+            return new Response();
+        }
+    }
+
+    public class Response
+    {
+        public string Message { get; set; } = "";
+        public int StatusCode { get; set; } = 200;
+    }
+}
